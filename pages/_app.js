b075d2accc5666a1ce041035890afe1fcc47c8a9
rayhannr <rayhannaufal2017@mail.ugm.ac.id>
2021-01-25 17:46:00 +0700

    maybe fix tat toggle button

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import '../tailwind.css'
 import '../styles/globals.css'
 import '../styles/variable.css'
 import '../styles/game.css'
+import '../styles/toggle.css'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
