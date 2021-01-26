a4592f71299d4a8c3cd62a0089c2d9d41d979f30
rayhannr <rayhannaufal2017@mail.ugm.ac.id>
2021-01-26 09:44:02 +0700

    change github and link icon color in project

--- components/ProjectLink.js
+++ components/ProjectLink.js
@@ -1,7 +1,13 @@
-const ProjectLink = props => (
-    <a href={props.link} target="_blank">
-        <svg xmlns="http://www.w3.org/2000/svg" width="28px" height="28px" viewBox="0 0 24 24" version="1.1"><title>External Link</title><g stroke="#111827" strokeWidth="1" fill="none" fillRule="evenodd" strokeLinecap="round"><path d="M19 13.012V19.4a1.6 1.6 0 0 1-1.6 1.6H4.6A1.6 1.6 0 0 1 3 19.4V6.6A1.6 1.6 0 0 1 4.6 5h6.452" /><path d="M14 3h6a1 1 0 0 1 1 1v6" /><path d="M20.5 3.5l-9.618 9.618" /></g></svg>
-    </a>
-)
+import { useContext } from 'react'
+import { ThemeContext } from '../context/theme-context'
+
+const ProjectLink = props => {
+    const theme = useContext(ThemeContext)
+    return (
+        <a href={props.link} target="_blank">
+            <svg xmlns="http://www.w3.org/2000/svg" width="28px" height="28px" viewBox="0 0 24 24" version="1.1"><title>External Link</title><g stroke={`${theme.isDark ? '#f3f4f6' : '#111827'}`} strokeWidth="1" fill="none" fillRule="evenodd" strokeLinecap="round"><path d="M19 13.012V19.4a1.6 1.6 0 0 1-1.6 1.6H4.6A1.6 1.6 0 0 1 3 19.4V6.6A1.6 1.6 0 0 1 4.6 5h6.452" /><path d="M14 3h6a1 1 0 0 1 1 1v6" /><path d="M20.5 3.5l-9.618 9.618" /></g></svg>
+        </a>
+    )
+}
 
 export default ProjectLink
